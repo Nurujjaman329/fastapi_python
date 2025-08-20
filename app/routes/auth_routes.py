4fd97674ac043afa6fb75952aed2790d13ac9bc1
Md Nurujjaman <mdnurujjaman329@gmail.com>
2025-08-20 16:00:26 +0600

    update : products part

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate, UserResponse, Login, Token
 from app.services.auth_service import AuthService
 from app.database import get_db
+from app.models.user import User
+
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -11,8 +13,12 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     service = AuthService(db)
     return service.register(user)
 
-
 @router.post("/login", response_model=Token)
 def login(user: Login, db: Session = Depends(get_db)):
     service = AuthService(db)
     return service.login(user)
+
+@router.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
--- app/schemas/product.py
+++ app/schemas/product.py
@@ -4,12 +4,14 @@ class ProductCreate(BaseModel):
     name: str
     description: str
     price: float
+    vendor_id: int   # add this
 
 class ProductResponse(BaseModel):
     id: int
     name: str
     description: str
     price: float
+    vendor_id: int   # add this
 
     class Config:
         orm_mode = True
